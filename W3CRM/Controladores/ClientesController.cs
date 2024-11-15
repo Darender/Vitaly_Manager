2d14abb443442a1241f87b234f7ba1477426466e
David Perez Escritorio <d1183203@uabc.edu.mx>
2024-11-14 19:03:15 -0800

    1.4.2

---
+++ W3CRM/Controladores/ClientesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vitaly_Manager.Controladores
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult AgregarClientes()
+        {
+            ClientesController controlador = new ClientesController();
+            return View(controlador);
+        }
+    }
+}
