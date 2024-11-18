854847ce78a234fe2095287cb95f63292bf66669
Serenity <kronossky.7@gmail.com>
2024-11-17 16:03:48 -0800

    PBI_PROD_US5
    Creacion de:
    Consulta de proveedores
    DataProveedores
    Controlador

--- W3CRM/Data/MainServidor.cs
+++ W3CRM/Data/MainServidor.cs
@@ -2,7 +2,11 @@
 {
     public static class MainServidor
     {
+<<<<<<< Updated upstream
         public static string Servidor = "Data Source=Alolo\\SQLEXPRESS;Initial Catalog=VitalyBD;Integrated Security=True;Encrypt=False";
+=======
+        public static string Servidor = "Data Source=DESKTOP-GFO28QN\\MSSQLSERVER01;Initial Catalog=VitalyBD;Integrated Security=True;Encrypt=False";
+>>>>>>> Stashed changes
 
 
 	}
