e9afb32c960bf23d787df725ace81da7fa14cb86
David Escritorio <74345962+Darender@users.noreply.github.com>
2024-11-28 19:51:04 -0800

    v0.10.1 - Corrección de errores

--- W3CRM/Data/MainServidor.cs
+++ W3CRM/Data/MainServidor.cs
@@ -2,7 +2,7 @@
 {
     public static class MainServidor
     {
-        public static string Servidor = "Data Source= DESKTOP-G480ROV;Initial Catalog=VitalyBD;Integrated Security=True;Encrypt=False";
+        public static string Servidor = "Data Source= DESKTOP-7NSJEG7\\SQLEXPRESS;Initial Catalog=VitalyBD;Integrated Security=True;Encrypt=False";
     }
 
 }
