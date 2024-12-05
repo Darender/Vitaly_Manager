b339ffe22ddc20138240fccb75e83ad8799f2d69
Serenity <kronossky.7@gmail.com>
2024-12-05 11:52:15 -0800

    PBI_COM_US1
    * Creación de el archivo de compras en la capa de vistas
    * Creación del controlador en la capa de controladores
    * Creación de la vista AgregarLoteProducto
    *Avance de ConsultarLoteProducto
    *Acomodo de tamaños en los registros de las interfaces
    *Acomodo de la tabla de ConsultaClientes
    *Agregar una nueva sección a SideBar

---
+++ W3CRM/Controladores/ComprasController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Vitaly_Manager.Data;
+using Vitaly_Manager.Entidades;
+
+namespace Vitaly_Manager.Controladores
+{
+    public class ComprasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AgregarLoteProducto()
+        {
+            ComprasController controlador = new ComprasController();
+            return View(controlador);
+        }
+
+        public IActionResult ConsultarLoteProducto()
+        {
+            ComprasController controlador = new ComprasController();
+            return View(controlador);
+        }
+    }
+}
