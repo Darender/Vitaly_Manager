7b6efd7a16e47a8adb77698d41243972f8f8d650
JorgeGarcia46 <jorge.garcia46@uabc.edu.mx>
2024-12-06 20:41:54 -0800

    Creacion de la clase DataGasto
    Se ha creado la clase DataGasto con las funciones que contendra, por el momento no tiene funcionalidad alguna.

---
+++ W3CRM/Data/DataGasto.cs
@@ -0,0 +1,27 @@
+﻿namespace Vitaly_Manager.Data
+{
+    public class DataGasto
+    {
+        public void AgregarGasto()
+        {
+
+        }
+
+        public void ModificarGasto()
+        {
+
+        }
+
+        public void EliminarGasto()
+        {
+
+        }
+
+        public void ConsultarGastos()
+        {
+
+        }
+
+        
+    }
+}
