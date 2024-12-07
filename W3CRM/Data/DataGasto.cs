b1fd1651ba283b50726c1500c4914a41136b75d0
JorgeGarcia46 <jorge.garcia46@uabc.edu.mx>
2024-12-07 12:40:07 -0800

    Codificación de la función EliminarGasto en DataGasto
    - Se ha codificado la función de EliminarGasto en la clase DataGasto.
    
    - Se ha documentado la función AgregarGasto.

--- W3CRM/Data/DataGasto.cs
+++ W3CRM/Data/DataGasto.cs
@@ -5,18 +5,24 @@ namespace Vitaly_Manager.Data
 {
     public class DataGasto
     {
+        //Agrega un gasto a la base de datos, toma como parametro el objeto tipo gasto que agregara
+        //y regresa un booleano y un string de respuesta
         public bool AgregarGasto(Gasto gasto, out string mensaje)
         {
             try
             {
+                //Se crea la conexión con la base de datos
                 using (SqlConnection conexion = new SqlConnection(MainServidor.Servidor))
                 {
+                    //Se abre la conexion
                     conexion.Open();
 
+                    //Se crea el query de insercion
                     string query = @"INSERT INTO Gasto 
                             (concepto, monto, fecha) 
                             VALUES (@Concepto, @Monto, @Fecha)";
 
+                    //Se ejecuta el query para insertar los datos del objeto en la tabla gasto
                     using (SqlCommand comando = new SqlCommand(query, conexion))
                     {
                         comando.Parameters.AddWithValue("@Concepto", gasto.concepto);
@@ -25,18 +31,23 @@ namespace Vitaly_Manager.Data
                         comando.ExecuteNonQuery();
                     }
 
+                    //Cierre de conexion
                     conexion.Close();
                 }
+
+                //se retorna true y un mensaje de exito
                 mensaje = $"El gasto ha sido agregado exitosamente.";
                 return true;
             }
             catch (SqlException ex)
             {
+                //Mensaje de error de Sql
                 mensaje = $"Error en la base de datos: {ex.Message}";
                 return false;
             }
             catch (Exception ex)
             {
+                //Mensaje de error del sistema
                 mensaje = $"Error inesperado: {ex.Message}";
                 return false;
             }
@@ -48,8 +59,54 @@ namespace Vitaly_Manager.Data
 
         }
 
-        public void EliminarGasto()
+        //Elimina un gasto de la base de datos, toma como parametro la id del gasto a eliminar y
+        //retorna un booleano y un string de respuesta.
+        public bool EliminarGasto(int idGasto, out string respuesta)
         {
+            try
+            {
+                //Se crea la conexión con la base de datos
+                using (SqlConnection conexion = new SqlConnection(MainServidor.Servidor))
+                {
+                    //Se abre la conexion
+                    conexion.Open();
+
+                    // Confirma que exista el id en la base de datos
+                    string queryExiste = $"SELECT COUNT(1) FROM Gasto WHERE IdGasto = {idGasto}";
+                    SqlCommand cmdExiste = new SqlCommand(queryExiste, conexion);
+                    int existe = (int)cmdExiste.ExecuteScalar();
+
+                    //Si no existe, se retorna false y un mensaje notifocandolo
+                    if (existe == 0)
+                    {
+                        respuesta = "No se encontro el gasto en la base de datos";
+                        return false;
+                    }
+
+                    //Se ejecuta el query de eliminación
+                    string queryEliminar = $"DELETE FROM Gasto WHERE IdGasto = {idGasto}";
+                    new SqlCommand(queryEliminar, conexion).ExecuteNonQuery();
+
+                    //Cierre de conexion
+                    conexion.Close();
+                }
+
+                //se retorna true y un mensaje de exito
+                respuesta = "Se elimino exitosamente el gasto";
+                return true;
+            }
+            catch (SqlException ex)
+            {
+                //Mensaje de error de Sql
+                respuesta = $"Error en la base de datos (SqlException): {ex.Message}";
+                return false;
+            }
+            catch (Exception ex)
+            {
+                //Mensaje de error del sistema
+                respuesta = $"Error inesperado (Exception): {ex.Message}";
+                return false;
+            }
 
         }
 
