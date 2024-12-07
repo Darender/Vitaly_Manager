80faa957fb8f1119de8c4c4e06d5003e1a790d43
JorgeGarcia46 <jorge.garcia46@uabc.edu.mx>
2024-12-06 19:41:28 -0800

    Creación de la clase Gasto en la capa entidad y mapeo de su respectiva tabla.

---
+++ W3CRM/Entidades/Gasto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Vitaly_Manager.Entidades
+{
+    public class Gasto
+    {
+        [Key]
+        public required int idServicioGeneral { get; set; }
+
+        [Required]
+        public required string concepto { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public required decimal monto { get; set; }
+
+        [Required]
+        public required DateTime fecha { get; set; }
+
+        public int? idProductoComprado {get;set;}
+
+
+    }
+}
