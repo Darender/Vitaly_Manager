d95b7de120fa0117a9dfc112a6718b309608d284
AntonioCruzMontes <antonio.cruz82@uabc.edu.mx>
2024-12-07 19:49:37 -0800

    vista funcional
    ligue la pagina al sidebar y ahora ya carga sin problemas, ahora solo sigue la funcionalidad

---
+++ W3CRM/Controladores/FinanzasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vitaly_Manager.Controladores
+{
+    public class FinanzasController : Controller
+    {
+        public ActionResult ConsultaGanancias()
+        {
+            return View(this);
+        }
+    }
+}
